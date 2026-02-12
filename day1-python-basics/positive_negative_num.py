9645a68ae847d99aa70db64d6cb83b9eeff631dc
machotjohn88-wq <machotjohn88@gmail.com>
2026-02-12 19:38:44 +0530

    Add files via upload

---
+++ day1-python-basics/positive_negative_num.py
@@ -0,0 +1,7 @@
+number=int(input("Enter a number: "))
+if number > 0:
+    print(f"The number, {number} , is positive.")
+elif number < 0:
+    print(f"The number, {number} , is negative.")
+else:
+    print("The number is zero.")
